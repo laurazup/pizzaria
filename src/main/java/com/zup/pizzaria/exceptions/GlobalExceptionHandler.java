21bd7a53f09eb6caec0944b4351b6afd86d8a3d0
LuizFernando <barbosa.luiz@zup.com.br>
2025-01-22 15:56:42 -0300

    feat: Adicionando a classe GlobalExceptionHandler para tratamento de exceçõe e validaçoes

---
+++ src/main/java/com/zup/pizzaria/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.zup.pizzaria.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.support.MetaDataAccessException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MetaDataAccessException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+}
